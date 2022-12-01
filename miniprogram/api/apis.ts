ce062c77412b191ff03e9e782fe6260c427b5a9f
wangyifeng <2665939276@qq.com>
2022-12-01 13:33:32 +0800

    feat：获用户信息接口

--- miniprogram/api/apis.ts
+++ miniprogram/api/apis.ts
@@ -5,6 +5,7 @@ export function requestOpenId(code: string) {
   return post(`/wx/auth?code=${code}`, {}, false)
 }
 
+// 获用户信息接口
 export function requestUserInfo(userId: string) {
   return get(`/wx/users/${userId}`, {}, false)
 }
